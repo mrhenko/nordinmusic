9f3b8a5b3034f831f7e6115199746a1736b8d7c9
Henrik Carlsson <henrik.carlsson@me.com>
2013-02-22 17:02:33 +0100

    Added a page template for the equipment page.

---
+++ page-equipment.php
@@ -0,0 +1,56 @@
+<?php
+	/* Template Name: Utrustning */
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<title><?php bloginfo('title'); ?> :: <?php the_title(); ?></title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<?php wp_enqueue_script("jquery"); ?>
+
+		<?php wp_head(); ?>
+	</head>
+
+	<body class="equipment"> 
+
+		<header id="logo" role="banner">
+			<h1><a href="<?php bloginfo('url'); ?>">
+				<span>Nordin</span> Music Productions
+				<?php /*bloginfo('title');*/ ?>
+			</a></h1>
+		</header><!-- #logo -->
+
+		<nav role="navigation">
+			<?php
+				wp_nav_menu(array(
+					'theme_location' => 'primary',
+					'depth' => 1,
+					'container' => false
+				));
+			?>
+		</nav><!-- The main navigation -->
+
+		<?php if ( have_posts() ) :?>
+			<?php while (have_posts() ) :?>
+				<?php the_post(); ?>
+				<article <?php post_class('main-content'); ?>>
+					<?php
+						the_content();
+						//print_r(mrh_process_image_tags(get_the_content()));
+					?>
+				</article>
+			<?php endwhile; ?>
+		<?php endif; ?>
+
+		<script src="<?php bloginfo('template_directory') ?>/gallery_images.js">
+		</script>
+		<?php wp_footer(); ?>
+	</body>
+
+</html>
