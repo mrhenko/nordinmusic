4c063008d93ffc5b2991404be9f76ea052c51a07
Henrik Carlsson <henrik.carlsson@me.com>
2013-02-26 10:56:53 +0100

    Started working on the Contact page.

---
+++ page-contact.php
@@ -0,0 +1,47 @@
+<?php
+	/* Template Name: Kontakt */
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+	<?php get_header(); ?>	
+
+	<body class="contact"> 
+
+		<header id="logo" role="banner">
+			<h1><a href="<?php bloginfo('url'); ?>">
+				<span>Nordin</span> Music Productions
+				<?php /*bloginfo('title');*/ ?>
+			</a></h1>
+		</header><!-- #logo -->
+
+		<nav role="navigation">
+			<?php
+				wp_nav_menu(array(
+					'theme_location' => 'primary',
+					'depth' => 1,
+					'container' => false
+				));
+			?>
+		</nav><!-- The main navigation -->
+
+		<?php if ( have_posts() ) :?>
+			<?php while (have_posts() ) :?>
+				<?php the_post(); ?>
+				<article <?php post_class('main-content'); ?>>
+					<?php
+						the_content();
+						//print_r(mrh_process_image_tags(get_the_content()));
+					?>
+				</article>
+			<?php endwhile; ?>
+		<?php endif; ?>
+
+		<script src="<?php bloginfo('template_directory') ?>/gallery_images.js">
+		</script>
+		<?php wp_footer(); ?>
+	</body>
+
+</html>
